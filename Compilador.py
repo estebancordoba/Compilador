a8855566d33e562b50b733f4e744379897165974
franco12594 <frank_franco12594@hotmail.com>
2015-10-10 08:40:40 -0500

    Update Compilador.py

--- Compilador.py
+++ Compilador.py
@@ -3,6 +3,8 @@
 # pyuic4 interfazCompilador.ui -o interfazCompilador.py
 
 import sys
+from warnings import catch_warnings
+from weakref import finalize
 from PyQt4 import QtCore, QtGui
 
 from interfazCompilador import *
@@ -11,8 +13,14 @@ ui_interfaz = Ui_compilador()
 
 
 class interfazCompilador(QtGui.QDialog):
-    def __init__(self):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        ui_interfaz.setupUi(self)
+
+        QtCore.QObject.connect(ui_interfaz.botonCompilar, QtCore.SIGNAL("clicked()"), self.eventoCompilar)
+
         self.recorridoLineas=0
+        self.recorridoLineasOtro=0
 
         self.compilo=False
 
@@ -29,12 +37,7 @@ class interfazCompilador(QtGui.QDialog):
         self.variables = ['v1', 'v2', 'v3', 'v4',
                           'v5', 'v6', 'v7', 'v8', 'v9']
 
-
-    def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
-        ui_interfaz.setupUi(self)
-
-        QtCore.QObject.connect(ui_interfaz.botonCompilar, QtCore.SIGNAL("clicked()"), self.eventoCompilar)
+        self.finales = ['f_pr']
 
     def eventoCompilar(self):
         self.cadenaPrincipal = ui_interfaz.textEditar.toPlainText()#Obtiene el texto a compilar
@@ -64,7 +67,6 @@ class interfazCompilador(QtGui.QDialog):
                         contEspacios-=1
                     else:
                         contEspacios+=1
-
                 if self.cadenaPalabras[0]=="FIN":
                     if self.compilo==True:
                         ui_interfaz.textErrores.setText("Compilo sin problemas :)")
@@ -75,8 +77,16 @@ class interfazCompilador(QtGui.QDialog):
                     self.compilo=False
                     return
 
+                if ((self.cadenaPalabras[0] in self.finales) == True):
+                    ui_interfaz.textErrores.setText("Existe un cierre de mas")
+                    self.compilo=False
+                    return
+
                 if self.cadenaPalabras[0]=="pr":
-                    self.eventoPara(self.envBloques("pr"))
+                    if (self.eventoPara(self.envBloques("pr"))==True):
+                        self.compilo=True
+                    else:
+                        return False
 
                 ##########################################################
                 # Aqui agregar la condicion para cada uno de los metodos #
@@ -93,11 +103,14 @@ class interfazCompilador(QtGui.QDialog):
         cadenaFinBloque=self.cadenaLineas[self.recorridoLineas].split(' ')
         cadenaBloque.append(cadenaFinBloque)
         finalCade="f_"+funcion
+
+        contRep= - 1
+
         while self.recorridoLineas < self.cadenaLineas.__len__():
             cadenaFinBloque=self.cadenaLineas[self.recorridoLineas].split(' ')
 
             contEspacios=0
-            while(contEspacios<cadenaFinBloque.__len__()):#Quitar espacios espacios sobrantes
+            while(contEspacios<cadenaFinBloque.__len__()-1):#Quitar espacios espacios sobrantes
                 if cadenaFinBloque[contEspacios] == "":
                     cadenaFinBloque.pop(contEspacios)
                     contEspacios-=1
@@ -106,9 +119,12 @@ class interfazCompilador(QtGui.QDialog):
 
             if self.cadenaLineas[self.recorridoLineas]=="FIN":
                 cadenaBloque.pop(0)
-                ui_interfaz.textErrores.setText("Error de Sintaxis (Falta "+str(finalCade)+" al final del bloque "+str(funcion)+")")
+                ui_interfaz.textErrores.setText("Error de Sintaxis (Falta "+str(finalCade)+" al final de un bloque "+str(funcion)+")")
                 self.compilo=False
                 return cadenaBloque
+
+            if cadenaFinBloque[0]==funcion:
+                contRep+=1
 
             if cadenaFinBloque[cadenaFinBloque.__len__()-1] != '.':
                 cadenaBloque.pop(0)
@@ -116,21 +132,125 @@ class interfazCompilador(QtGui.QDialog):
                 self.compilo=False
                 return cadenaBloque
 
-            if(cadenaFinBloque[0]==finalCade):
+            if(cadenaFinBloque[0]==finalCade and contRep==0):
+                if (cadenaFinBloque[1]!='.' or cadenaFinBloque.__len__()>2):
+                    cadenaBloque.pop(0)
+                    ui_interfaz.textErrores.setText("Error de Sintaxis (El final para "+str(funcion)+" no corresponde")
+                    self.compilo=False
+                    return cadenaBloque
                 cadenaBloque.append(cadenaFinBloque)
                 cadenaBloque.pop(0)
                 ui_interfaz.textErrores.clear()
                 self.compilo=True
                 return cadenaBloque
+
             else:
+                if contRep>0 and cadenaFinBloque[0]==finalCade:
+                    contRep-=1
+                    if (cadenaFinBloque[1]!='.' or cadenaFinBloque.__len__()>2):
+                        cadenaBloque.pop(0)
+                        ui_interfaz.textErrores.setText("Error de Sintaxis (El final para "+str(funcion)+" no corresponde")
+                        self.compilo=False
+                        return cadenaBloque
                 cadenaBloque.append(cadenaFinBloque)
             self.recorridoLineas += 1
 
 
     def eventoPara(self, cadenaPara):
         if self.compilo == True:
+            # pr < v1 = 0 , 5 , + 1 > .
+            print("<------------------------------------------------->")
+
             for i in cadenaPara:
                 print(i)
+
+
+            if (cadenaPara[0][0]=="pr"):
+                if (cadenaPara[0][1]=="<"):
+                    if ((cadenaPara[0][2] in self.variables)==True):
+                        if (cadenaPara[0][3]=="="):
+                            if ((cadenaPara[0][4] in self.numeros)==True):
+                                if (cadenaPara[0][5]==","):
+                                    if ((cadenaPara[0][6] in self.numeros)==True):
+                                        if (cadenaPara[0][7]==","):
+                                            if (cadenaPara[0][8]=="+" or cadenaPara[0][8]=="-"):
+                                                if ((cadenaPara[0][9] in self.numeros)==True):
+                                                    if (cadenaPara[0][10]==">"):
+                                                        if (cadenaPara[0][11]=="."):
+                                                            self.compilo = True
+                                                        else:
+                                                            self.compilo = False
+                                                    else:
+                                                        self.compilo = False
+                                                else:
+                                                    self.compilo = False
+                                            else:
+                                                self.compilo = False
+                                        else:
+                                            self.compilo = False
+                                    else:
+                                        self.compilo = False
+                                else:
+                                    self.compilo = False
+                            else:
+                                self.compilo = False
+                        else:
+                            self.compilo = False
+                    else:
+                        self.compilo = False
+                else:
+                    self.compilo = False
+            else:
+                self.compilo = False
+
+            if self.compilo == True:
+
+                self.recorridoLineasOtro=1
+                while self.recorridoLineasOtro<cadenaPara.__len__()-1:
+
+                    if cadenaPara[self.recorridoLineasOtro][0]=="pr":
+                        if (self.eventoPara(self.enviarOtroBloque("pr", cadenaPara))==True):
+                            return True
+                        else:
+                            return False
+                    ##########################################################
+                    # Aqui agregar la condicion para cada uno de los metodos #
+                    ##########################################################
+
+                    self.recorridoLineasOtro+=1
+                return True
+
+
+            else:
+                ui_interfaz.textErrores.setText("Error, la gramatica de pr no consiste")
+
+                return False
+
+
+    def enviarOtroBloque(self, funcion, cadenaOtra):
+
+        finalCade="f_"+funcion
+        cadenaFinal=[]
+        contRep= - 1
+
+        self.compilo=False
+
+        while(self.recorridoLineasOtro<cadenaOtra.__len__()-1):
+
+            if cadenaOtra[self.recorridoLineasOtro][0] == funcion:
+                contRep+=1
+            if(cadenaOtra[self.recorridoLineasOtro][0]==finalCade and contRep==0):
+                cadenaFinal.append(cadenaOtra[self.recorridoLineasOtro])
+                ui_interfaz.textErrores.clear()
+                self.compilo=True
+                return cadenaFinal
+
+            else:
+                if contRep>0 and cadenaOtra[self.recorridoLineasOtro][0]==finalCade:
+                    contRep-=1
+                cadenaFinal.append(cadenaOtra[self.recorridoLineasOtro])
+
+            self.recorridoLineasOtro+=1
 
     #############################################################
     # Aqui agregar los metodos para cada uno de las condiciones #
